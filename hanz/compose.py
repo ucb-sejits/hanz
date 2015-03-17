9dd20526289a50f795c4e84a19a60c66bbc0fa94
Leonard Truong <leonardtruong@gmail.com>
2015-03-17 10:57:30 -0700

    Add composable block finder

--- hanz/compose.py
+++ hanz/compose.py
@@ -78,13 +78,47 @@ def unpack(expr):
     return visit(expr)
 
 
-class ControlFlowGraph(object):
+op2str = {
+    ast.Add: "+",
+    ast.Mult: "*",
+    ast.Sub: "-",
+    ast.Div: "/"
+}
 
+
+def dump_op(op):
+    if isinstance(op, ast.Call):
+        return "{}({})".format(op.func.id, ", ".join([arg.id for arg in
+                                                      op.args]))
+    elif isinstance(op, ast.BinOp):
+        return "{} {} {}".format(op.left.id, op2str[op.op.__class__],
+                                 op.right.id)
+    else:
+        raise NotImplementedError(op)
+
+
+class ComposableBlock(object):
+    def __init__(self):
+        self.statements = []
+
+    def add_statement(self, statement):
+        self.statements.append(statement)
+
+    def dump(self, tab):
+        output = tab + "ComposableBlock\n"
+        tab += "    "
+        for expr in self.statements:
+            if isinstance(expr, ast.Assign):
+                output += tab + "{} = {}\n".format(
+                    expr.targets[0].id, dump_op(expr.value))
+        return output
+
+
+class ControlFlowGraph(object):
     """
     A datastructure to represent the control flow graph of a function as a
     graph of basic blocks.
     """
-
     def __init__(self, func):
         """
         :param ast.FunctionDef func:
@@ -94,23 +128,6 @@ class ControlFlowGraph(object):
         body = map(unpack, func.body)
         self.basic_blocks = [list(reduce(lambda x, y: x + y, body, ()))]
 
-    op2str = {
-        ast.Add: "+",
-        ast.Mult: "*",
-        ast.Sub: "-",
-        ast.Div: "/"
-    }
-
-    def dump_op(self, op):
-        if isinstance(op, ast.Call):
-            return "{}({})".format(op.func.id, ", ".join([arg.id for arg in
-                                                          op.args]))
-        elif isinstance(op, ast.BinOp):
-            return "{} {} {}".format(op.left.id, self.op2str[op.op.__class__],
-                                     op.right.id)
-        else:
-            raise NotImplementedError(op)
-
     def __str__(self):
         output = ""
         tab = "    "
@@ -119,20 +136,102 @@ class ControlFlowGraph(object):
             for expr in block:
                 if isinstance(expr, ast.Assign):
                     output += tab + "{} = {}\n".format(
-                        expr.targets[0].id, self.dump_op(expr.value))
+                        expr.targets[0].id, dump_op(expr.value))
                 elif isinstance(expr, ast.Return):
                     output += tab + "return {}\n".format(expr.value.id)
+                elif isinstance(expr, ComposableBlock):
+                    output += expr.dump(tab)
         return output
 
+    def compile_to_fn(self, env):
+        # TODO: Should we create a new module/funcdef or just reuse the one
+        # passed in
+        tree = ast.Module(
+            [ast.FunctionDef(self.name, self.params,
+                             self.basic_blocks[0], [])]
+        )
+        ast.fix_missing_locations(tree)
+        exec(compile(tree, filename=self.name, mode="exec"), env, env)
+        return env[self.name]
 
-def compile_cfg(graph, env):
-    tree = ast.Module(
-        [ast.FunctionDef(graph.name, graph.params,
-                         graph.basic_blocks[0], [])]
-    )
-    ast.fix_missing_locations(tree)
-    exec(compile(tree, filename="file", mode="exec"), env, env)
-    return env[graph.name]
+    def append_composable(self, new_block, specializer, statement,
+                          symbol_table, args):
+        if len(new_block) < 1 or \
+                not isinstance(new_block[-1], ComposableBlock):
+            new_block.append(ComposableBlock())
+        new_block[-1].add_statement(statement)
+        target = statement.targets[0].id
+        symbol_table[target] = specializer.eval_symbolically(*args)
+
+    binop2attr = {
+        ast.Add: "__add__",
+        ast.Mult: "__mul__",
+        ast.Sub: "__sub__",
+        ast.Div: "__div__"
+    }
+
+    binop2rattr = {
+        ast.Add: "__radd__",
+        ast.Mult: "__rmul__",
+        ast.Sub: "__rsub__",
+        ast.Div: "__rdiv__"
+    }
+
+    def separate_composable_ops(self, block, symbol_table):
+        new_block = []
+        for statement in block:
+            if isinstance(statement, ast.Assign):
+                if isinstance(statement.value, ast.Call):
+                    # TODO: Could be attribute
+                    func = symbol_table[statement.value.func.id]
+                    if hasattr(func, '_specializer'):
+                        args = (symbol_table[arg.id]
+                                for arg in statement.value.args)
+                        self.append_composable(new_block, func._specializer,
+                                               statement, symbol_table, args)
+                    else:
+                        new_block.append(statement)
+                elif isinstance(statement.value, ast.BinOp):
+                    attr = self.binop2attr[statement.value.op.__class__]
+                    rattr = self.binop2rattr[statement.value.op.__class__]
+                    if statement.value.left.id in symbol_table and \
+                            statement.value.right.id in symbol_table:
+                        left = symbol_table[statement.value.left.id]
+                        right = symbol_table[statement.value.right.id]
+                        left_func = getattr(left, attr)
+                        right_func = getattr(right, rattr)
+                        if hasattr(left_func, 'specialized_dispatch'):
+                            func = left_func.fn(left, right)
+                            self.append_composable(new_block,
+                                                   func._specializer,
+                                                   statement, symbol_table,
+                                                   (left, right))
+                        elif hasattr(right_func, 'specialized_dispatch'):
+                            func = right_func.fn(right, left)
+                            self.append_composable(new_block,
+                                                   func._specializer,
+                                                   statement, symbol_table,
+                                                   (right, left))
+                        else:
+                            new_block.append(statement)
+                    else:
+                        new_block.append(statement)
+                else:
+                    new_block.append(statement)
+            elif isinstance(statement, ast.Return):
+                new_block.append(statement)
+            else:
+                raise NotImplementedError(statement)
+        return new_block
+
+    def find_composable_blocks(self, symbol_table):
+        new_basic_blocks = []
+        for block in self.basic_blocks:
+            new_block = self.separate_composable_ops(block, symbol_table)
+            new_basic_blocks.append(new_block)
+        self.basic_blocks = new_basic_blocks
+        print(self)
+        raise NotImplementedError()
 
 
 def compose(fn):
@@ -143,9 +242,14 @@ def compose(fn):
     symbol_table.update(frame.f_globals)
     symbol_table.update(frame.f_back.f_locals)
     symbol_table.update(frame.f_back.f_globals)
-    print(cfg)
 
     def wrapped(*args, **kwargs):
-        fn = compile_cfg(cfg, symbol_table)
+        # Make a mutable copy of closure
+        env = dict(symbol_table)
+        # Load parameters into environment
+        for index, arg in enumerate(tree.body[0].args.args):
+            env[arg.id] = args[index]
+        cfg.find_composable_blocks(env)
+        fn = cfg.compile_to_fn(env)
         return fn(*args, **kwargs)
     return wrapped
